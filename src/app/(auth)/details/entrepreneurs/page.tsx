addf5953e15d2d17d0b98fcf366cda5583c9b388
Alaa Deban <alaa.deban@merwas.com>
2024-07-10 13:28:11 +0300

    the skeleton of details is ready

--- src/app/(auth)/details/entrepreneurs/page.tsx
+++ src/app/(auth)/details/entrepreneurs/page.tsx
@@ -3,17 +3,12 @@ import type { NextPage } from 'next';
 
 import { Box, Button, Grid, Paper } from '@mui/material';
 import Link from 'next/link';
-import { loginBgImage, whoAreYouBgImage } from '@/constant/images';
-import styles from '../sign-in/page.module.css';
+import { loginBgImage } from '@/constant/images';
+import styles from '../../sign-in/page.module.css';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-import { useEffect, useState } from 'react';
-import { ToggleButtonGroup, Button as JoyButton } from '@mui/joy';
-import whoAreStyles from './page.module.css';
 
 const WhoAreYouPage: NextPage = () => {
-  const [type, setType] = useState('student');
-  console.log(type);
   return (
     <div className={styles.signInContainer}>
       <div className="w-full ">
@@ -41,60 +36,12 @@ const WhoAreYouPage: NextPage = () => {
               </div>{' '}
               <div className=" sm-flex-row-row-center-center">
                 <p className=" text-large-title m-1">
-                  Who Are <span className="fc-secondary">You</span>
+                  <span className="fc-secondary">Entrepreneurs</span> Details!
                 </p>
               </div>
-              <div className="sm-flex-col-col-center-center">
-                <ToggleButtonGroup
-                  orientation="vertical"
-                  variant="plain"
-                  value={type}
-                  onChange={(event, newType) => {
-                    setType(newType as string);
-                  }}
-                  aria-label="text alignment"
-                >
-                  <JoyButton
-                    value="student"
-                    aria-label="student"
-                    className={whoAreStyles.singleButtonGroup}
-                    style={{
-                      backgroundColor:
-                        type === 'student' ? 'orange' : 'transparent',
-                      color: type === 'student' ? 'white' : 'black',
-                    }}
-                  >
-                    Student
-                  </JoyButton>
-                  <JoyButton
-                    value="mentor"
-                    aria-label="mentor"
-                    className={whoAreStyles.singleButtonGroup}
-                    style={{
-                      backgroundColor:
-                        type === 'mentor' ? 'orange' : 'transparent',
-                      color: type === 'mentor' ? 'white' : 'black',
-                    }}
-                  >
-                    Mentor
-                  </JoyButton>
-                  <JoyButton
-                    value="entrepreneurs"
-                    aria-label="entrepreneurs"
-                    className={whoAreStyles.singleButtonGroup}
-                    style={{
-                      backgroundColor:
-                        type === 'entrepreneurs' ? 'orange' : 'transparent',
-                      color: type === 'entrepreneurs' ? 'white' : 'black',
-                    }}
-                  >
-                    Entrepreneurs
-                  </JoyButton>
-                </ToggleButtonGroup>
-                <p className="fc-secondary">Please select only one</p>
-              </div>
+              <div className="sm-flex-col-col-center-center"></div>
               <div className=" sm-flex-row-row-center-between  w-full mt-2 ">
-                <Link href={'/sign-up'}>
+                <Link href={'/who-are-you'}>
                   <Button
                     variant="outlined"
                     sx={{
@@ -113,7 +60,7 @@ const WhoAreYouPage: NextPage = () => {
                     Back
                   </Button>
                 </Link>
-                <Link href={`/details/${type}`}>
+                <Link href={'/who-are-you'}>
                   <Button
                     variant="outlined"
                     sx={{
@@ -130,7 +77,7 @@ const WhoAreYouPage: NextPage = () => {
                     }}
                     endIcon={<KeyboardDoubleArrowRightIcon />}
                   >
-                    Next
+                    Submit
                   </Button>
                 </Link>
               </div>
@@ -142,7 +89,7 @@ const WhoAreYouPage: NextPage = () => {
             sm={4}
             md={6}
             sx={{
-              backgroundImage: 'url("/who-are-you-bg.png")',
+              backgroundImage: 'url("/details.png")',
               backgroundColor: 'gray',
               backgroundSize: 'cover',
               backgroundPosition: 'center',
--- src/constant/images.tsx
+++ src/constant/images.tsx
@@ -1,3 +1,4 @@
 export const loginBgImage = '/logo3.svg';
 export const registerBgImage = '/register-background.svg';
 export const whoAreYouBgImage = '/who-are-are-bg.png';
+export const detailsBgImage = '/details.png';
