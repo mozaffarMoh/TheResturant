ebc64e1c739672a9f955aba8f9a63adccdbd43e2
Alaa Deban <alaa.deban@merwas.com>
2024-07-15 01:27:53 +0300

    partner section done

--- src/constant/images.tsx
+++ src/constant/images.tsx
@@ -3,9 +3,11 @@ export const registerBgImage = '/register-background.svg';
 export const whoAreYouBgImage = '/who-are-are-bg.png';
 export const detailsBgImage = '/details.png';
 
-
 export const aboutUsSectionImage = '/about-us-section.png';
 export const servicesSectionImage = '/services-bg.png';
 
 export const successCheckMark = '/green-check.png';
 
+export const partnerImage1 = '/partner/partner1.png';
+export const partnerImage2 = '/partner/partner2.png';
+export const partnerImage3 = '/partner/partner3.png';
