f13dadf30549671556f5573d8ef58cff044894a8
Mozaffar Mohammad <134216611+mozaffarMoh@users.noreply.github.com>
2024-08-13 09:26:35 +0300

    add-social-media

--- src/components/cards/contact-us-section/contactUsSectionCard.tsx
+++ src/components/cards/contact-us-section/contactUsSectionCard.tsx
@@ -1,8 +1,11 @@
 'use client';
 import { domain } from '@/base-api/endPoints';
 import { greyBackground } from '@/constant/images';
-import { Grid, Typography } from '@mui/material';
-import Cookies from 'js-cookie';
+import { Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+import styles from '../../../sections/guest-home/contact-us/contact-us.module.css';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface IProps {
   title: string;
@@ -10,8 +13,9 @@ interface IProps {
   media: any;
 }
 const ContactUsSectionCard = ({ title, content, media }: IProps) => {
-  const langCookie = Cookies.get('NEXT_LOCALE') || 'en';
-  const isRTL = langCookie == 'ar';
+  const pathname = usePathname();
+  let isArabic = pathname.startsWith('/ar');
+  const isScreen500 = useMediaQuery('(max-width:500px)');
 
   let imageURL =
     media && media.length > 0 && media[0]?.url
@@ -41,11 +45,54 @@ const ContactUsSectionCard = ({ title, content, media }: IProps) => {
       >
         <p className="text-med-low  ">{title}</p>
         <Typography
-          sx={{ textAlign: isRTL ? 'right' : 'left' }}
+          sx={{ textAlign: isArabic ? 'right' : 'left' }}
           className="fc-light-black-new"
         >
-          {content && content[0] && content[0]?.value}
+          {content && content.length == 1 && content[0]?.value}
         </Typography>
+
+        {content && content.length > 1 && (
+          <Grid
+            container
+            wrap="nowrap"
+            spacing={2}
+            sx={{ marginLeft: '', marginTop: '1rem' }}
+          >
+            <Stack
+              direction={'row'}
+              position={'relative'}
+              spacing={isScreen500 ? 1 : 2}
+              gap={1}
+              style={{ marginRight: isArabic ? '-10px' : '' }}
+            >
+              {content.map((item: any) => {
+                const imageURL =
+                  item?.media && item?.media.length == 1
+                    ? domain + item?.media[0]?.url
+                    : '';
+                return (
+                  <Stack
+                    key={item.id}
+                    className={styles.socialIconContainer}
+                    padding={isScreen500 ? 0 : 0.5}
+                  >
+                    <Link
+                      href={item.value}
+                      target="_blank"
+                    >
+                      <Image
+                        src={imageURL}
+                        alt="social-icon"
+                        width={25}
+                        height={25}
+                      />
+                    </Link>
+                  </Stack>
+                );
+              })}
+            </Stack>
+          </Grid>
+        )}
       </Grid>
     </Grid>
   );
--- src/sections/guest-home/contact-us/contactUsSection.tsx
+++ src/sections/guest-home/contact-us/contactUsSection.tsx
@@ -1,27 +1,16 @@
-import { Container, Grid, Stack, useMediaQuery } from '@mui/material';
+import { Container, Grid, useMediaQuery } from '@mui/material';
 import styles from './contact-us.module.css';
-import {
-  FaceBookMedSVG,
-  InstagramMedSVG,
-  LinkedInMedSVG,
-  ShareSVG,
-  SkypeMedSVG,
-  TwitterMedSVG,
-} from '../../../../assets/icons';
 import ContactUsSectionCard from '@/components/cards/contact-us-section/contactUsSectionCard';
 import ContactUsForm from './contact-us-form/contactUsForm';
 import { useTranslations } from 'next-intl';
 import useGet from '@/custom-hooks/useGet';
 import { endPoints } from '@/base-api/endPoints';
 import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
 
 const ContactUsSection = () => {
   const isScreen700 = useMediaQuery('(max-width:700px)');
   const t = useTranslations();
   const [data, , getData] = useGet(endPoints.contactUsDetials);
-  const pathname = usePathname();
-  let isArabic = pathname.startsWith('/ar');
 
   useEffect(() => {
     getData();
@@ -71,9 +60,7 @@ const ContactUsSection = () => {
 
               <Grid
                 container
-                className="mt-2 "
-                spacing={2}
-                rowSpacing={2}
+                rowSpacing={5}
               >
                 {data &&
                   data?.children &&
@@ -91,51 +78,6 @@ const ContactUsSection = () => {
                       />
                     </Grid>
                   ))}
-
-                <Grid
-                  container
-                  wrap="nowrap"
-                  spacing={2}
-                  sx={{ marginLeft: '0.7rem', marginTop: '1rem' }}
-                >
-                  <Grid
-                    item
-                    sx={{ marginTop: '1.5rem' }}
-                  >
-                    <ShareSVG />
-                  </Grid>
-                  <Grid
-                    item
-                    xs
-                  >
-                    <p className="text-med-low  ">
-                      {t('contact-us.follow-us')}
-                    </p>
-
-                    <Stack
-                      direction={'row'}
-                      position={'relative'}
-                      left={isArabic ? 12 : -12}
-                      gap={1}
-                    >
-                      <div className={styles.socialIconContainer}>
-                        <TwitterMedSVG />
-                      </div>
-                      <div className={styles.socialIconContainer}>
-                        <FaceBookMedSVG />
-                      </div>
-                      <div className={styles.socialIconContainer}>
-                        <InstagramMedSVG />
-                      </div>
-                      <div className={styles.socialIconContainer}>
-                        <LinkedInMedSVG />
-                      </div>
-                      <div className={styles.socialIconContainer}>
-                        <SkypeMedSVG />
-                      </div>
-                    </Stack>
-                  </Grid>
-                </Grid>
               </Grid>
             </Grid>
           </Grid>
@@ -145,7 +87,7 @@ const ContactUsSection = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387194.07372680923!2d-74.31001987831878!3d40.69701731549496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sro!4v1722500622223!5m2!1sen!2sro"
             width="100%"
             height={isScreen700 ? '300' : '450'}
-            style={{ border: 0 }}
+            style={{ border: 0, marginTop: '30px' }}
             allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
